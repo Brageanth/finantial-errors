397010de11f5bfc6c24f9b964db74e1b44d9898e
boni <x.bonillaa@gmail.com>
2020-10-12 16:46:43 -0500

    :sparkles:

--- src/components/sectionM.tsx
+++ src/components/sectionM.tsx
@@ -4,8 +4,10 @@ import "../styles/components/sectionM.css"
 export default function SectionM() {
   return (
       <div className="sectionm">
-        <h2>"Tanto si crees que puedes como si crees que no puedes, estas en lo cierto"</h2>
-        <h2 className="light">Henry Ford</h2>
+        <div className="contenedor">
+          <h2>"Tanto si crees que puedes como si crees que no puedes, estas en lo cierto"</h2>
+          <h2 className="light">Henry Ford</h2>
+        </div>
       </div>
   );
 }
