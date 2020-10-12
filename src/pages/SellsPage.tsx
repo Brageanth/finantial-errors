4b75d4945d20a4f6d4516f81bd87b33fb51d00b3
boni <x.bonillaa@gmail.com>
2020-10-12 03:04:44 -0500

    :sparkles:

--- src/pages/SellsPage.tsx
+++ src/pages/SellsPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BannerTop from "../components/bannerTop";
 import SectionM from "../components/sectionM";
+import BoniChallenge from "../components/bonichallenge"
 
 export default function SellsPage() {
   return (
     <>
       <BannerTop />
       <SectionM />
+      <BoniChallenge />
     </>
   );
 }
