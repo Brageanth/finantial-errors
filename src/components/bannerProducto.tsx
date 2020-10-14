ff023ee9aa22c275a9dc3db0b6b0f4cb03ba43a5
Brageanth David <brageanth@outlook.es>
2020-10-14 02:01:36 -0500

    :lipstick: update css

--- src/components/bannerProducto.tsx
+++ src/components/bannerProducto.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import "../styles/components/bannerProducto.css";
 
 export default function BannerProducto() {
-  return <div className = "fondoBanner">
-      <h1>3 temas VITALES que el colegio NO está enseñando a nuestros hijos</h1>
-      <h3>Entérate de cuáles son esos temas y cómo llenar ese grave vacio del sistema escolar</h3>
+  return (
+    <div className="fondoBanner">
+      <h1>
+        3 temas VITALES que el colegio <span>NO está enseñando</span> a nuestros
+        hijos
+      </h1>
+      <h3>
+        Entérate de cuáles son esos temas y cómo llenar ese grave vacio del
+        sistema escolar.
+      </h3>
       <img src="https://finantial-errors-cdn.s3.amazonaws.com/img/Logo+ILYA.png" />
-  </div>;
+    </div>
+  );
 }
