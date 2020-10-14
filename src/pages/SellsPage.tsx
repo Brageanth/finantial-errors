75381c0ed5ae99d2bfb6865f985744b866e31c75
Julian Rique <julielmejor15@hotmail.com>
2020-10-14 01:25:27 -0500

    ✨Subo 3 commponentes en desarrollo

--- src/pages/SellsPage.tsx
+++ src/pages/SellsPage.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import BannerProducto from "../components/bannerProducto";
 import BannerTop from "../components/bannerTop";
+import ProductVideo from "../components/productVideo";
+import SecondPartInfo from "../components/secondPartInfo";
 import SectionM from "../components/sectionM";
+import GirlPart from "../components/girlPart";
+
 
 export default function SellsPage() {
   return (
     <>
       <BannerTop />
       <SectionM />
+      <SecondPartInfo />
+      <BannerProducto />
+      <ProductVideo />
+      <GirlPart />
     </>
   );
 }
