fdfbd804b35d14e48cc56511401acaa3d1c20cd8
Brageanth David <brageanth@outlook.es>
2020-10-14 02:59:50 -0500

    :lipstick: Styles girlPart

--- src/components/avisoPeque.tsx
+++ src/components/avisoPeque.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import "../styles/components/avisoPeque.css";
 
 export default function BannerProducto() {
-  return <div className="barraSupe">
-      <h1>¿En qué consiste <span>Los Niños y El Dinero</span>?</h1>
+  return (
+    <div className="barraSupe">
+      <h1>
+        ¿En qué consiste <span>Los Niños y El Dinero</span>?
+      </h1>
       <br></br>
-      <h2>Los Niños y El Dinero es un curso online con más de 40 lecciones 
-          donde vamos a enseñarte TODO lo que hemos aprendido en estos 13 años y nos 
-          hemos llevado de experiencia en más de 1.300 entrenamientos dictados con 
-          más de 300.000 estudiantes.</h2>
-  </div>;
+      <h2>
+        Los Niños y El Dinero es un curso online con más de 40 lecciones donde
+        vamos a enseñarte TODO lo que hemos aprendido en estos 13 años y nos
+        hemos llevado de experiencia en más de 1.300 entrenamientos dictados con
+        más de 300.000 estudiantes.
+      </h2>
+    </div>
+  );
 }
